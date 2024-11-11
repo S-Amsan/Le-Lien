676ab6d24fb339a7d2b12373b10ef7ffae07f4ff
S-Amsan <165779415+S-Amsan@users.noreply.github.com>
2024-11-11 21:56:57 +0100

    Ajout de DOMContentLoaded

--- Le Lien/assets/js/header.js
+++ Le Lien/assets/js/header.js
@@ -17,36 +17,37 @@ if (pageActuel === pages.accueil){ //Car la page d'acceuil n'est pas au meme emp
 else{
     pages.accueil.href = "../" + pages.accueil.href;
 }
-
-document.querySelector('header').innerHTML = `
-    <nav class="navbar">
-        <div class="navbar-gauche">
-            <img src="${pageActuel.srcImg}/Logo.png" alt="Logo Le lien" class="logo">
-        </div>
-        <div class="navbar-droite">
-            <a class="${pages.accueil.class}" href="${pages.accueil.href}">Accueil</a>
-            <a class="${pages.presentation.class}"  href="${pages.presentation.href}">Présentation</a>
-            <a class="${pages.nosActions.class}"  href="${pages.nosActions.href}">Nos Actions</a>
-            <a class="${pages.declarerAccident.class}"  id="accident" href="${pages.declarerAccident.href}">Déclarez un accident</a>
-            <div class="boutons-navbar">
-                <button class="bouton bouton-aider" onclick="window.location.href='${pages.nousAider.href}';">Nous aider</button>
-                <button class="bouton bouton-recherche">
-                    <img src="${pageActuel.srcImg}/logo-recherche.png" alt="Logo pour recherche" class="logo-recherche">
-                    Rechercher
-                </button>
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('header').innerHTML = `
+        <nav class="navbar">
+            <div class="navbar-gauche">
+                <img src="${pageActuel.srcImg}/Logo.png" alt="Logo Le lien" class="logo">
             </div>
-        </div>
-        <button class="menu-burger" id="menu-burger">
-            <img src="../assets/images/menuBurger.png" alt="menuBurger" width="100%" height="100%" class="menu-burger">
-        </button>
-    </nav>
-
-    <div class="menu-mobile" id="menu-mobile">
-        <a href="../accueil.html">Accueil</a>
-        <a href="presentation.html">Présentation</a>
-        <a href="#">Nos Actions</a>
-        <a href="Declarer%20un%20accident.html">Déclarez un accident</a>
-        <button class="bouton bouton-aider">Nous aider</button>
-        <button class="bouton bouton-recherche">Rechercher</button>
-    </div>       
-`
+            <div class="navbar-droite">
+                <a class="${pages.accueil.class}" href="${pages.accueil.href}">Accueil</a>
+                <a class="${pages.presentation.class}"  href="${pages.presentation.href}">Présentation</a>
+                <a class="${pages.nosActions.class}"  href="${pages.nosActions.href}">Nos Actions</a>
+                <a class="${pages.declarerAccident.class}"  id="accident" href="${pages.declarerAccident.href}">Déclarez un accident</a>
+                <div class="boutons-navbar">
+                    <button class="bouton bouton-aider" onclick="window.location.href='${pages.nousAider.href}';">Nous aider</button>
+                    <button class="bouton bouton-recherche">
+                        <img src="${pageActuel.srcImg}/logo-recherche.png" alt="Logo pour recherche" class="logo-recherche">
+                        Rechercher
+                    </button>
+                </div>
+            </div>
+            <button class="menu-burger" id="menu-burger">
+                <img src="../assets/images/menuBurger.png" alt="menuBurger" width="100%" height="100%" class="menu-burger">
+            </button>
+        </nav>
+    
+        <div class="menu-mobile" id="menu-mobile">
+            <a href="../accueil.html">Accueil</a>
+            <a href="presentation.html">Présentation</a>
+            <a href="#">Nos Actions</a>
+            <a href="Declarer%20un%20accident.html">Déclarez un accident</a>
+            <button class="bouton bouton-aider">Nous aider</button>
+            <button class="bouton bouton-recherche">Rechercher</button>
+        </div>       
+    `
+});
