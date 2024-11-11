341ad9a5654419fa8fb939810ee31edd1660cea8
S-Amsan <165779415+S-Amsan@users.noreply.github.com>
2024-11-11 21:40:46 +0100

    Modification des chemins vers les images
    le chemins vers les images n'était pas correctement mis, il pointer vers un fichier png que j'avais supprimé.

--- Le Lien/assets/js/data.js
+++ Le Lien/assets/js/data.js
@@ -36,13 +36,13 @@ export const contenuCarrouselData = [
   <h3>Fondateurs</h3>
   <div class="personnes">
     <div class="personne">
-      <img src="../assets/images/personne.png" alt="">
+      <img src="../assets/images/personnes/Alain-Michel Ceretti.png" alt="">
       <p class="nom">Alain-Michel Ceretti</p>
       <p class="description">Conseiller santé auprès du Défenseur des droits, actuellement président d’honneur du LIEN.</p>
     </div>
     <div class="personne">
-      <img src="../assets/images/personne.png" alt="">
-      <p class="nom">Jean-Luc Bernard </p>
+      <img src="../assets/images/personnes/Jean-Luc Bernard.png" alt="">
+      <p class="nom">Jean-Luc Bernard</p>
       <p class="description">1er président du CISS (Collectif Inter-Associatif sur la Santé).</p>
     </div>
   </div>
@@ -51,22 +51,22 @@ export const contenuCarrouselData = [
   <h3>Membres du bureau</h3>
   <div class="personnes">
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/Chantal Cateau.png" alt="">
         <p class="nom">Chantal Cateau</p>
         <p class="description">Présidente</p>
       </div>
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/Claude Rambaud.png" alt="">
         <p class="nom">Claude Rambaud</p>
         <p class="description">Vice-présidente</p>
       </div>
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/François Rullier.png" alt="">
         <p class="nom">François Rullier</p>
         <p class="description">Trésorier</p>
       </div>
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/Jean-François Baibled.png" alt="">
         <p class="nom">Jean-François Baibled</p>
         <p class="description">Secrétaire général</p>
       </div>
@@ -76,22 +76,22 @@ export const contenuCarrouselData = [
   <h3>Membres du bureau</h3>
   <div class="personnes">
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/Nicole Billast.png" alt="">
         <p class="nom">Nicole Billast</p>
         <p class="description">Poste non précisé</p>
       </div>
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/Lorraine Brière de L’Isle.png" alt="">
         <p class="nom">Lorraine Brière de L’Isle</p>
         <p class="description">Poste non précisé</p>
       </div>
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/Jean-Pierre Thierry.png" alt="">
         <p class="nom">Jean-Pierre Thierry</p>
         <p class="description">Conseiller médical</p>
       </div>
       <div class="personne">
-        <img src="../assets/images/personne.png" alt="">
+        <img src="../assets/images/personnes/Alain-Michel Ceretti.png" alt="">
         <p class="nom">Alain-Michel Ceretti</p>
         <p class="description">Président d’honneur</p>
       </div>
