a9753e23390612802204f3fa1fb4d8410a7c1ef6
S-Amsan <165779415+S-Amsan@users.noreply.github.com>
2024-12-04 16:01:05 +0100

    Delete Le Lien/assets/js/pageDon.js

--- Le Lien/assets/js/pageDon.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.getElementById('menu-burger');
-    const mobileMenu = document.getElementById('menu-mobile');
-
-    burgerMenu.addEventListener('click', function () {
-        // Toggle l'affichage du menu mobile
-        mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
-    });
-});
