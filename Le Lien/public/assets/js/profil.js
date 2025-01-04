af1144de1a357ed338f4dd2d148141d3e1897705
Lucas <lucasbarbier80@gmail.com>
2025-01-04 23:41:39 +0100

    couleur rang dynamique

--- Le Lien/public/assets/js/profil.js
+++ Le Lien/public/assets/js/profil.js
@@ -10,12 +10,15 @@ userName.textContent = await getNom() + " " + await getPrenom();
 if(await utilisateurEstAdmin()){
     logoRang.src = "../assets/images/profil/logoRang/couronne.png";
     rangUser.textContent = "Admin";
+    rangUser.classList.add("admin");
 }
 else if(await utilisateurEstAdherent()){
     logoRang.src = "../assets/images/profil/logoRang/adherent-logo.png";
     rangUser.textContent = "Adhérent";
+    rangUser.classList.add("adherent");
 }
 else{
     logoRang.src = "../assets/images/profil/logoRang/utilisateur-logo.png";
     rangUser.textContent = "Utilisateur";
+    rangUser.classList.add("utilisateur");
 }
